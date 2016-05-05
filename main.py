4ed2430c5a0218684904ea68585aae1cfda1bfd5
Rômulo Farias <romulodefarias@gmail.com>
2016-05-04 21:32:30 -0300

    Code quality improvements

--- main.py
+++ main.py
@@ -3,8 +3,12 @@
 
 # imports
 from slugify import slugify
-from collections import Counter
-import fnmatch, os, sys, argparse, re, json, php
+import fnmatch
+import os
+import argparse
+import re
+import json
+import php
 
 # header of file to get args
 parser = argparse.ArgumentParser(description='Create translator files for laravel.')
@@ -12,37 +16,37 @@ parser.add_argument('from_path', metavar='from_path', type=str, help='Path where
 parser.add_argument('prefix', metavar='prefix', type=str, help='the prefix of translator function')
 parser.add_argument('destiny', metavar='destiny', type=str, help='the destiny file')
 args = parser.parse_args()
-	
 
 # main scope
 main_exp = re.compile(r'<(?!(?:script)|(?:img)|(?:input))[a-z0-9]+[^>]*>([^<>]+)<\/[a-z0-9]+>')
 not_exp = re.compile('(@[a-z-A-Z0-9]+(\((.)*\))?)|({{.*}})')
 
 try:
-	to_file = open(args.destiny+'.map', 'r')
-	mapper = json.loads(to_file.read())
-	to_file.close()
+    to_file = open(args.destiny + '.map', 'r')
+    mapper = json.loads(to_file.read())
+    to_file.close()
 except ValueError and FileNotFoundError:
-	mapper = {}
+    mapper = {}
+
 
 # functions
 def add_to_mapper(mainmapper, mylist, value):
-	key = mylist[0]
-	try:
-		if type(mainmapper[key]) is dict and not len(mylist) == 1:
-			add_to_mapper(mainmapper[key], mylist[1:], value)
-		else:
-			mainmapper[key] = value
-	except KeyError:
-		mainmapper[key] = create_map_from_list(mylist[1:], value)
+    key = mylist[0]
+    try:
+        if type(mainmapper[key]) is dict and not len(mylist) == 1:
+            add_to_mapper(mainmapper[key], mylist[1:], value)
+        else:
+            mainmapper[key] = value
+    except KeyError:
+        mainmapper[key] = create_map_from_list(mylist[1:], value)
 
 
 def create_map_from_list(mylist, value):
-	if len(mylist) == 0:
-		return value
-	else:
-		key = mylist[0]
-		return {key: create_map_from_list(mylist[1:], value) }
+    if len(mylist) == 0:
+        return value
+    else:
+        key = mylist[0]
+        return {key: create_map_from_list(mylist[1:], value)}
 
 
 # Find files
@@ -55,80 +59,77 @@ file_count = 0
 
 # Find Strings
 for filename in matches:
-	file_count += 1
+    file_count += 1
 
-	file = open(filename, 'r')
-	matches_count = 0
+    file = open(filename, 'r')
+    matches_count = 0
 
-	filemapper = {}
-	
-	try:
-		file_content = file.read()
-		file.close()
-		search = main_exp.findall(file_content)		
-		for row in search:	
-			row = row.replace("\n", " ").strip()
-			if not not_exp.match(row) and len(row) > 3:
-				matches_count += 1
+    filemapper = {}
 
-				keys = row.split(' ')
-				keys = list(filter(lambda word: len(word.replace('-', '').replace('*', '').strip()) > 0, keys))
-				keys = keys[:6]
-				keys = list(map(slugify, keys))
+    try:
+        file_content = file.read()
+        file.close()
+        search = main_exp.findall(file_content)
+        for row in search:
+            row = row.replace("\n", " ").strip()
+            if not not_exp.match(row) and len(row) > 3:
+                matches_count += 1
 
-				filemapper['-'.join(keys)] = row
+                keys = row.split(' ')
+                keys = list(filter(lambda word: len(word.replace('-', '').replace('*', '').strip()) > 0, keys))
+                keys = keys[:6]
+                keys = list(map(slugify, keys))
 
-	except AttributeError as e:
-		print('File ' + filename + ' ERROR: ' + str(e))
-		continue
-	except UnicodeError as e:
-		print('File ' + filename + ' ERROR: ' + str(e))
-		continue
+                filemapper['-'.join(keys)] = row
 
-	mapperPath = filename.replace(args.from_path,'').replace('.blade.php','').replace('/','\\').strip()
-	if mapperPath[0] == '\\':
-		mapperPath = mapperPath[1:]
+    except AttributeError as e:
+        print('File ' + filename + ' ERROR: ' + str(e))
+        continue
+    except UnicodeError as e:
+        print('File ' + filename + ' ERROR: ' + str(e))
+        continue
 
-	mapperPath = mapperPath.split('\\')
-	mapperPath = list(map(slugify, mapperPath))
+    mapperPath = filename.replace(args.from_path, '').replace('.blade.php', '').replace('/', '\\').strip()
+    if mapperPath[0] == '\\':
+        mapperPath = mapperPath[1:]
 
-	add_to_mapper(mapper, mapperPath, filemapper)
+    mapperPath = mapperPath.split('\\')
+    mapperPath = list(map(slugify, mapperPath))
 
-	if file_count > 0:
-		try:
-			file = open(filename+'.bak', 'w+')
-			file.write(file_content)
-			file.close()
-		except UnicodeError as e:
-			print(e)
+    add_to_mapper(mapper, mapperPath, filemapper)
 
-		prefix = args.prefix + '.'.join(mapperPath)
-		for key in filemapper:
-			try:
-				laravel_code = "{{ trans('" + prefix + "." + key + "') }}"
-				file_content = file_content.replace(filemapper[key], laravel_code)
-			except UnicodeError as e:
-				print(e)			
+    if file_count > 0:
+        try:
+            file = open(filename + '.bak', 'w+')
+            file.write(file_content)
+            file.close()
+        except UnicodeError as e:
+            print(e)
 
-		try:
-			file = open(filename, 'w+')
-			file.write(file_content)
-			file.close()
-		except UnicodeError as e:
-			print(e)
-		
-	print('File ' + filename + ' - Matches ' + str(matches_count))
+        prefix = args.prefix + '.'.join(mapperPath)
+        for key in filemapper:
+            try:
+                laravel_code = "{{ trans('" + prefix + "." + key + "') }}"
+                file_content = file_content.replace(filemapper[key], laravel_code)
+            except UnicodeError as e:
+                print(e)
 
-to_file = open(args.destiny+'.map', 'w+')
+        try:
+            file = open(filename, 'w+')
+            file.write(file_content)
+            file.close()
+        except UnicodeError as e:
+            print(e)
+
+    print('File ' + filename + ' - Matches ' + str(matches_count))
+
+to_file = open(args.destiny + '.map', 'w+')
 to_file.write(json.dumps(mapper))
-#o_file.write(json.dumps(mapper, sort_keys=True, indent=4, separators=(',', ': '))) # beauty
+# o_file.write(json.dumps(mapper, sort_keys=True, indent=4, separators=(',', ': '))) # beauty
 to_file.close()
 
 print()
 print(str(file_count) + " files")
 
-
 php_array = php.serialize(mapper)
 php_array.to_file(args.destiny)
-
-
--- php.py
+++ php.py
@@ -1,42 +1,36 @@
 class serialize:
+    def __init__(self, dictionary):
+        self.__data = []
+        for key in list(dictionary):
+            self.__data.append(array(key, dictionary[key]))
 
-	def __init__ (self, dictionary):
-		self.__data = []
-		for key in list(dictionary):
-			self.__data.append(array(key, dictionary[key]))
+    def to_file(self, filename):
+        file = open(filename, 'w+')
+        string = '<?php\n\n'
+        string += 'return ' + str(self) + ';'
+        file.write(string)
+        file.close()
 
+    def __str__(self):
+        string = "[\n"
 
-	def to_file(self, filename):
-		file = open(filename, 'w+')
-		string = '<?php\n\n'
-		string += 'return ' + str(self) + ';'
-		file.write(string)
-		file.close()
+        for row in self.__data:
+            string += str(row) + ",\n"
 
-	def __str__(self):
-		string = "[\n"
-
-		for row in self.__data:
-			string += str(row) + ",\n"
-
-
-		string += "\n]"
-		return string
+        string += "\n]"
+        return string
 
 
 class array:
+    def __init__(self, key, value):
+        self.__key = key
+        if type(value) is dict:
+            self.__value = serialize(value)
+        else:
+            self.__value = value
 
-	def __init__(self, key, value):
-			self.__key = key
-			if type(value) is dict:
-				self.__value = serialize(value)
-			else:
-				self.__value = value
-
-	def __str__(self):
-		if type(self.__value) is serialize:
-			return "'" + self.__key + "' => " + str(self.__value)
-		else:
-			return "'" + self.__key + "' => '" + str(self.__value).replace("'", "\\'") + "'"
-
-
+    def __str__(self):
+        if type(self.__value) is serialize:
+            return "'" + self.__key + "' => " + str(self.__value)
+        else:
+            return "'" + self.__key + "' => '" + str(self.__value).replace("'", "\\'") + "'"
